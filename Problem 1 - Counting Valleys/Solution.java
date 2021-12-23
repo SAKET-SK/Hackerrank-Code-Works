6cee9e6adaaec9ad04b8e0352345170e63e4c3cc
Saket Khopkar <saketkhopkar478@gmail.com>
2021-12-23 11:41:40 +0530

    Update Solution.java

--- Problem 1 - Counting Valleys/Solution.java
+++ Problem 1 - Counting Valleys/Solution.java
@@ -22,17 +22,17 @@ class Result {
      */
 
     static int countingValleys(int n, String s) {
-    int sum = 0;
-    int count = 0;
-    for(int i=0;i<n;i++){
-    if(s.charAt(i)=='U'){
-      if(++sum==0)
-        count++;
-    }
-    else sum--;
-  }
-  return count;
-}
+        int sum = 0;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='U'){
+                if(++sum==0)
+                    count++;
+             }
+            else sum--;
+        }
+        return count;
+     }
 }
 
 public class Solution {
